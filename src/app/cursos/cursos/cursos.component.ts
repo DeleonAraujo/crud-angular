f259eaddc121858a570901f99d339c30a3a045c3
DeleonAraujo <deleon.rj@outlook.com>
2022-07-05 18:25:59 -0300

    aula 11

--- src/app/cursos/cursos/cursos.component.ts
+++ src/app/cursos/cursos/cursos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { MatDialog } from '@angular/material/dialog';
+import { catchError, Observable, of } from 'rxjs';
+import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
 
 import { CursosService } from '../services/cursos.service';
 import { Curso } from './../model/curso';
@@ -14,11 +16,22 @@ export class CursosComponent implements OnInit {
   cursos$: Observable<Curso[]>;
   displayedColumns = ['nome', 'categoria'];
 
-  constructor(private cursosService : CursosService) {
-    this.cursos$ = this.cursosService.list();
+  constructor(private cursosService : CursosService,
+    public dialog: MatDialog) {
+    this.cursos$ = this.cursosService.list().pipe(catchError(error => {
+      this.onError('Erro ao carregar os cursos.');
+      return of ([])
+    })
+    );
    }
+
+   onError(errorMsg: string) {
+    this.dialog.open(ErrorDialogComponent, {
+      data: errorMsg
+    });
+  }
+
 
   ngOnInit(): void {
   }
-
 }
